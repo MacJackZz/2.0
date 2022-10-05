43534c77b57f6b00e4d85e647f0e41199daa7bec
MacJackZz <senior.hendrawan@gmail.com>
2022-10-05 22:18:00 +0700

    del bbrp func, del data gk guna, btn back berhasil

--- data.py
+++ data.py
@@ -15,68 +15,10 @@ kategori_10 = "Sejarah & Geografi"
 kategori_1_list = ['Dasar-Dasar Teknik Informatika', 'Pengantar Teknologi Informasi - Teknik Informatika', 'Metode Penelitian Teknik Informatika', 'Komputer Cerdas Untuk Mahasiswa Teknik Informatika', 'Teknik Pengolahan Audio & Video', 'Pengantar Teknologi Informatika dan Komunikasi Data', 'Aplikasi Komputer']
 
 
+kategori_2_list = [
+    'Loving The Wounded Soul', 'Filosofi Teras', 
+]
 
-k_1_buku1 = {
-    'judul' : 'Dasar-Dasar Teknik Informatika',
-    'penulis' : 'Novega Pratama Adiputra',
-    'halaman' : 117,
-    'isbn' : '978-623-02-0819-5',
-}
-
-
-k_1_buku2 = {
-    'judul' : 'Pengantar Teknologi Informasi - Teknik Informatika',
-    'penulis' : 'Buhori Muslim',
-    'halaman' : 392,
-    'isbn' : '978-602-453-242-0',
-}
-
-k_1_buku3 = {
-    'judul' : 'Metode Penelitian Teknik Informatika',
-    'penulis' : 'Ade Johar Maturidi',
-    'halaman' : 135,
-    'isbn' : '978-602-8981-72-9',
-}
-
-k_1_buku4 = {
-    'judul' : 'Komputer Cerdas Untuk Mahasiswa Teknik Informatika',
-    'penulis' : 'Nur Nafi’iyah',
-    'halaman' : 243,
-    'isbn' : '978-602-401-976-1',
-}
-
-k_1_buku5 = {
-    'judul' : 'Teknik Pengolahan Audio & Video',
-    'penulis' : 'Johnie Rogers Swanda Pasaribu, S.Kom., M.Kom.',
-    'halaman' : 171,
-    'isbn' : '978-623-02-0713-6',
-}
-
-k_1_buku6 = {
-    'judul' : 'Pengantar Teknologi Informatika dan Komunikasi Data',
-    'penulis' : 'Bagaskoro, S.Kom., M.M.',
-    'halaman' : 121,
-    'isbn' : '978-623-209-852-7',
-}
-
-k_1_buku7 = {
-    'judul' : 'Aplikasi Komputer',
-    'penulis' : 'Dwi Krisbiantoro',
-    'halaman' : 167,
-    'isbn' : '978-602-475-992-6',
-}
-
-
-
-
-
-kategori_1_dict = {
-    'b1' : k_1_buku1,
-    'b2' : k_1_buku2,
-    'b3' : k_1_buku3,
-    # 'b4' : k_1_buku4,
-    # 'b5' : k_1_buku5,
-    # 'b6' : k_1_buku6,
-    # 'b7' : k_1_buku7,
-    
-}
+kategori_3_list = [
+    'Buku Agama 1', 'Buku Agama 2', 'Buku Agama 3', 'Buku Agama 4','Buku Agama 5', 'Buku Agama 6'
+]
