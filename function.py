06351f945085941c8ea68acbe9fdc68eff3a302d
MacJackZz <senior.hendrawan@gmail.com>
2022-10-22 23:10:56 +0700

    import os. clear & dokumentasi setiap function

--- function.py
+++ function.py
@@ -1,7 +1,9 @@
 import os
 
-# Fungsi untuk tampilan menu awal
+
 def printHalamanAwal() :
+    ''' Fungsi untuk tampilan menu awal'''
+
     # Biar auto clear
     os.system('cls')
 
@@ -28,8 +30,9 @@ def printHalamanAwal() :
     """) 
 
 
-# Fungsi untuk print Header sesuai dengan kategorinya masing-masing
+
 def printKategoriHeader (kategori) :
+    '''Fungsi untuk print Header sesuai dengan kategorinya masing-masing'''
     os.system('cls')
 
     print("-".center(67,"-"))
@@ -37,16 +40,17 @@ def printKategoriHeader (kategori) :
     print("-".center(67,"-"))
     print(kategori.center(67))
     print("-".center(67,"-"))
-     
-# Menampilkan list buku sesuai dengan no. kategori dalam parameter
+
 def tampilListBuku (kategori_list_no):
+    '''Menampilkan list buku sesuai dengan no. kategori dalam parameter'''
     number = 1
     for judul in kategori_list_no :
       print(f"{number}. {judul}")
       number +=1
 
-# Function pilihanKategoriBuku
+
 def pilihanKategoriBuku (userInput, no_pilihan, kategori_ke, kategori_list_ke) :
+    '''Function untuk setiap pilihan kategori bukunya masing-masing'''
     while(userInput == no_pilihan) :
     
         printKategoriHeader(kategori_ke)
@@ -65,8 +69,10 @@ def pilihanKategoriBuku (userInput, no_pilihan, kategori_ke, kategori_list_ke) :
 
 
 def tampilPesanKeluar() :
+    ''' Fungsi untuk pesan ketika user keluar dari aplikasi '''
     os.system('cls')
-    
+    print("\n"*7)
     print("-".center(67, "-"))
     print("Anda telah keluar dari Aplikasi!".center(67))
     print("-".center(67, "-"))
+    print("\n"*7)
